17b323e53c239f04c5bcd7c6b86028bf22727bec
OrionHQI <anointedogbebor98@gmail.com>
2026-09-22 13:57:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -351,10 +351,7 @@ if (continueNumberButton && countrySelect && serviceSelect && numberMessage) {
 
 try {
     const response = await fetch(
-        "https://wisdomprosms-backend.onrender.com/number-offers?country=" +
-        encodeURIComponent(country) +
-        "&service=" +
-        encodeURIComponent(service),
+        `https://wisdomprosms-backend.onrender.com/number-offers?country=${encodeURIComponent(country)}&service=${encodeURIComponent(service)}`,
         {
             method: "GET",
             headers: {
